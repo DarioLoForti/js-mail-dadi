3c0d604424337bcaf4f76f6e88825d46791d2f20
DarioLoForti <lofortidario@gmail.com>
2023-11-23 14:50:15 +0100

    [mail]

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,9 +1,25 @@
 // 1 - creao un arrey con diverse email di confronto
 
-let email = ["pippo@libero.it", "pluto@libero.it", "ciccio@libero.it, franco@libero.it"];
-console.log(email);
+let emails = ["pippo@libero.it", "pluto@libero.it", "ciccio@libero.it", "franco@libero.it"];
+console.log(emails);
 
 // 2 - chiedo tramite promt all'utente di inserire la propria email
 
-let my_email =prompt("Inserisci la tua email")
-console.log(my_email);
+let my_email =prompt("Inserisci la tua email");
+
+
+// 3 - confronto che l'email inserita nel promt sia presente tra quelle dell'arrey
+
+for(let i = 0; i<emails.length; i++){
+    // console.log(emails[i])
+
+    if(my_email === emails[i]){
+        
+        console.log("Accesso confermato");
+    }
+
+    else{
+        console.log("Acceesso negato");
+    }
+    
+}
